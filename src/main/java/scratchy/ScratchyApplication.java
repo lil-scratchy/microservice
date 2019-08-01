7609241889d132051837030aac2da9b97df156bf
Ingo Kuba <ikuba@intension.de>
2019-08-01 09:24:35 +0200

    Spring application with mariadb.

---
+++ src/main/java/scratchy/ScratchyApplication.java
@@ -0,0 +1,14 @@
+package scratchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchyApplication
+{
+
+    public static void main(String... args)
+    {
+        SpringApplication.run(ScratchyApplication.class, args);
+    }
+}
